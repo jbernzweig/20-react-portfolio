02e37723324e0969e995d29143e2b3e0fbb0c4ed
Jason Bernzweig <jasonbernzweig@Jasons-Air.lan>
2023-06-12 16:48:32 -0400

    Im pretty stupid for taking this bootcamp

---
+++ src/components/Navbar/index.js
@@ -0,0 +1,15 @@
+// import './index.css';
+
+// const Navbar = () => <>Hello from Navbar</>
+
+// export default Navbar
+
+// <header className="App-header">
+//         <h1>Jason Bernzweig</h1>
+//         <nav>
+//           <a href="#about">About Me</a>
+//           <a href="#work">Work</a>
+//           <a href="#contact">Contact</a>
+//           <a href="#resume">Resume</a>
+//         </nav>
+//       </header>
